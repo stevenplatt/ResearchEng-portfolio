a9cf3b39d62d1e22225a459dd29bd22629b90e7c
telecomsteve <mr.platt@gmail.com>
2024-12-26 18:12:25 -0500

    directory refactor

--- js/nav-loader.js
+++ js/nav-loader.js
@@ -1,6 +1,6 @@
 // nav-loader.js
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('./html/sidenav.html') 
+    fetch('../sidenav.html') 
       .then(response => response.text())
       .then(data => {
         document.getElementById('nav-placeholder').innerHTML = data;
